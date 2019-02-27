673a7a6c90b4b2703b32f57ae469196e58ca6025
qiaoxingxing <1620998366@qq.com>
2019-02-27 22:06:19 +0800

    合成测试

--- BaiCiZhan.UnitTest/UnitTest1.cs
+++ BaiCiZhan.UnitTest/UnitTest1.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BaiCiZhan.Helper;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.IO;
+using NAudio.MediaFoundation;
 
 namespace BaiCiZhan.UnitTest
 {
@@ -21,6 +25,100 @@ namespace BaiCiZhan.UnitTest
             //IHistoryHelper hisHelper = new HistoryHelper();
             //hisHelper.Add("test");
             //var a = hisHelper.GetAll();
+        }
+        [TestMethod]
+        public void TestCreateFile()
+        {
+            //mixing 同时播放了;
+            using (var reader1 = new AudioFileReader(@"D:\desktop\wav\1.mp3"))
+            using (var reader2 = new AudioFileReader(@"D:\desktop\wav\2.mp3"))
+            {
+                var mixer = new MixingSampleProvider(new[] { reader1, reader2 });
+                WaveFileWriter.CreateWaveFile16(@"D:\desktop\wav\12.mp3", mixer);
+            }
+        }
+
+        [TestMethod]
+        public void TestCombineFile()
+        {
+            FileStream fileStream = new FileStream(@"D:\desktop\wav\12.mp3", FileMode.OpenOrCreate);
+            var files = new String[] { @"D:\desktop\wav\1.mp3",
+                @"D:\desktop\wav\2.mp3",
+                 @"D:\desktop\wav\2.aac"};
+            Combine(files, fileStream);
+        }
+
+
+        [TestMethod]
+        public void testConvert()
+        {
+            MediaFoundationApi.Startup();
+            String aacfile = @"D:\desktop\wav\2.aac";
+            String mp3file = @"D:\desktop\wav\1.mp3";
+            var wmafile = @"D:\desktop\wav\123.wma";
+            using (var reader = new MediaFoundationReader(aacfile))
+            {
+                MediaFoundationEncoder.EncodeToWma(reader, wmafile);
+            }
+        }
+        [TestMethod]
+        public void testConvert1()
+        {
+            //qxx cant convert MP3 file by this way       
+            MediaFoundationApi.Startup();
+            String aacfile = @"D:\desktop\wav\2.aac";
+            String mp3file = @"D:\desktop\wav\1.mp3";
+            String outfile = @"D:\desktop\wav\123.aac";
+            using (var reader = new MediaFoundationReader(mp3file))
+            {
+                MediaFoundationEncoder.EncodeToMp3(reader, outfile);
+            }
+        }
+
+        [TestMethod]
+        public void testConvertMp3()
+        {
+            var infile = @"D:\desktop\wav\1.mp3";
+            var outfile = @"D:\desktop\wav\14.wav";
+            using (var reader = new Mp3FileReader(infile))
+            {
+                WaveFileWriter.CreateWaveFile(outfile, reader);
+            }
+        }
+
+
+        public static void Combine(string[] inputFiles, Stream output)
+        {
+            foreach (string file in inputFiles)
+            {
+                Mp3FileReader reader = new Mp3FileReader(file);
+                if ((output.Position == 0) && (reader.Id3v2Tag != null))
+                {
+                    output.Write(reader.Id3v2Tag.RawData, 0, reader.Id3v2Tag.RawData.Length);
+                }
+
+                Mp3Frame frame;
+                while ((frame = reader.ReadNextFrame()) != null)
+                {
+                    output.Write(frame.RawData, 0, frame.RawData.Length);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void testMerge()
+        {
+            AudioHelper audiohelper = new AudioHelper();
+            audiohelper.TempDir = @"D:\desktop\wav\output";
+            //Directory.Delete(audiohelper.TempDir,true);
+            Directory.CreateDirectory(audiohelper.TempDir);
+
+            var file1 = audiohelper.Mp3ToWav(@"D:\desktop\wav\1.mp3");
+            var file2 = audiohelper.AacToWav(@"D:\desktop\wav\2.aac");
+            var files = new String[] { file1, file2 };
+            audiohelper.MergeWavs(files, @"D:\desktop\wav\output\1.wav");
+
+
         }
     }
 }
---
+++ BaiCiZhan/Helper/AudioHelper.cs
@@ -0,0 +1,102 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NAudio.MediaFoundation;
+
+namespace BaiCiZhan.Helper
+{
+    public class AudioHelper
+    {
+        public String TempDir { set; get; }
+
+        public AudioHelper()
+        {
+            TempDir = @"C:\Users\qxxx\AppData\Local\Temp";
+        }
+        public void MergeWavs(IEnumerable<string> sourceFiles,string outputFile)
+        {
+            byte[] buffer = new byte[1024];
+            WaveFileWriter waveFileWriter = null;
+
+            try
+            {
+                foreach (string sourceFile in sourceFiles)
+                {
+                    using (WaveFileReader reader = new WaveFileReader(sourceFile))
+                    {
+                        if (waveFileWriter == null)
+                        {
+                            // first time in create new Writer
+                            waveFileWriter = new WaveFileWriter(outputFile, reader.WaveFormat);
+                        }
+                        else
+                        {
+                            if (!reader.WaveFormat.Equals(waveFileWriter.WaveFormat))
+                            {
+                                throw new InvalidOperationException("Can't concatenate WAV Files that don't share the same format");
+                            }
+                        }
+
+                        int read;
+                        while ((read = reader.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            waveFileWriter.WriteData(buffer, 0, read);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                if (waveFileWriter != null)
+                {
+                    waveFileWriter.Dispose();
+                }
+            }
+
+        }
+
+        WaveFormat newFormat = new WaveFormat(8000, 16, 2);
+
+        public String Mp3ToWav(String infile, String outFile = "")
+        {
+            if (String.IsNullOrEmpty(outFile))
+            {
+                Guid guid = Guid.NewGuid();
+                outFile = Path.Combine(TempDir, guid.ToString() + ".wav");
+            }
+           
+
+            using (var reader = new Mp3FileReader(infile))
+            using (var conversionStream = new WaveFormatConversionStream(newFormat, reader))
+
+            {
+                WaveFileWriter.CreateWaveFile(outFile, conversionStream);
+            }
+            return outFile;
+        }
+
+        public String AacToWav(String infile, String outFile = "")
+        {
+            MediaFoundationApi.Startup();
+            if (String.IsNullOrEmpty(outFile))
+            {
+                Guid guid = Guid.NewGuid();
+                outFile = Path.Combine(TempDir, guid.ToString() + ".wav");
+            }
+            using (var reader = new MediaFoundationReader(infile))
+            using (var conversionStream = new WaveFormatConversionStream(newFormat, reader))
+            {
+                //MediaFoundationEncoder.EncodeToWma(reader, outFile);
+                WaveFileWriter.CreateWaveFile(outFile, conversionStream);
+            }
+
+            return outFile;
+        }
+
+
+    }
+}
